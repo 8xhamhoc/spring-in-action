cd1ab06e7a3f76d081967e3b8ce25b8ae135fb7f
8xquang <quang.phan@smartdev.vn>
2019-04-21 11:11:28 +0700

    chapter3: Spring @Profile.

---
+++ src/main/java/com/myapp/DataSourceConfig.java
@@ -0,0 +1,38 @@
+package com.myapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    @Profile("dev")
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
+
+    /**
+     * The bean won't be created unless the prod is active
+     * */
+    @Bean
+    @Profile("prod")
+    public DataSource jndiDataSource() {
+        JndiObjectFactoryBean jndiObjectFactoryBean = new JndiObjectFactoryBean();
+        jndiObjectFactoryBean.setJndiName("jdbc/myDS");
+        jndiObjectFactoryBean.setResourceRef(true);
+        jndiObjectFactoryBean.setProxyInterface(DataSource.class);
+        return (DataSource) jndiObjectFactoryBean.getObject();
+    }
+
+}
