0d7154f592f44f49f944a79fdc5a093317068acb
8xquang <quang.phan@smartdev.vn>
2019-05-05 15:41:34 +0700

    chapter3: Bean scope.

---
+++ src/main/java/com/myapp/Notepad.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class Notepad {
+
+    // the details of this class are inconsequential to this example
+    
+}
